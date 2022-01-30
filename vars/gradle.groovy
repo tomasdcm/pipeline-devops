8a9ef5513a7a183e394683e36c6b418f97a8097c
tomasdcm <65799599+tomasdcm@users.noreply.github.com>
2022-01-30 17:17:11 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -14,8 +14,8 @@ def call(){
             
                     /*def scannerHome = tool 'sonar-scanner';
                     withSonarQubeEnv('sonar-server') { 
-                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " */
-                    }
+                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build "
+                    }*/
            
         }
         

b39f1f2e3e7ed4fc9a46f417c8d0bffbddfd1cb1
tomasdcm <65799599+tomasdcm@users.noreply.github.com>
2022-01-30 17:10:44 -0300

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,43 +1,50 @@
-def call() {
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
+
+def call(){
+  
+  		stage('Compile Code') {            
+            		bat "mvn clean compile -e"
+        }
         
-        stage('Compile') {
+        stage('SonarQube analysis') {
+            
+                    def scannerHome = tool 'sonar-scanner';
+                    withSonarQubeEnv('sonar-server') { 
+                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
+                    }
+           
+        }
+        
+         stage('Test Code') {           
+                    bat "mvn clean test -e" 
+        }
+
+         stage('Jar Code') {
+           
+                    bat "mvn clean package -e"            
+        }
+
+         stage('Run Jar') {           
+                    bat "start /min mvn spring-boot:run &"           
+        }
+        
+        /*stage('Test Application') {
             steps {
                 script{
-                        echo 'Compile'
-                    }
-                }
+                    bat "start chrome http://localhost:8081/rest/mscovid/test?msg=testing" 
+                }                                    
             }
+        }*/
         
-        stage('Test') {
-            steps {
-                script{
-                        echo 'Test'
-                    }
-                }
-            }
-        
-        stage('Jar') {
-            steps {
-                script{
-                        echo 'Jar'
-                    }
-                }
-            }
-        
-        stage('Run') {
-            steps {
-                script{
-                        echo 'Run'
-                    }
-                }
-            }
-        
-        stage('TestApp') {
-            steps {
-                    echo 'TestApp'
-                }
-            }
-        
+        stage('Upload Nexus') {   
+                       echo 'Nexus stage'
+                   // bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
+        }
+
 }
 
 return this;

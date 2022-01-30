fd115baa6df946158b0a73b3350d67584aa0cce6
tomasdcm <65799599+tomasdcm@users.noreply.github.com>
2022-01-30 20:34:01 -0300

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -4,46 +4,41 @@
 	ejecucion.call()
 */
 
-def call(){
-  
-  		stage('Compile Code') {            
-            		bat "mvn clean compile -e"
-        }
-        
-        stage('SonarQube analysis') {
-            
-                    def scannerHome = tool 'sonar-scanner';
-                    withSonarQubeEnv('sonar-server') { 
-                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
-                    }
-           
-        }
-        
-         stage('Test Code') {           
-                    bat "mvn clean test -e" 
-        }
+def call(String pipelineType){
 
-         stage('Jar Code') {
-           
-                    bat "mvn clean package -e"            
-        }
+  figlet 'Maven'
 
-         stage('Run Jar') {           
-                    bat "start /min mvn spring-boot:run &"           
-        }
-        
-        /*stage('Test Application') {
-            steps {
-                script{
-                    bat "start chrome http://localhost:8081/rest/mscovid/test?msg=testing" 
-                }                                    
-            }
-        }*/
-        
-        stage('Upload Nexus') {   
-                       echo 'Nexus stage'
-                   // bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
-        }
+  if (pipelineType == 'CI'){
+    figlet 'Integracion Continua'
+
+    stage('Compile') {
+      STAGE = env.STAGE_NAME
+      sh './mvnw clean compile -e'
+    }
+
+    stage('Test') {
+      STAGE = env.STAGE_NAME
+      sh './mvnw clean test -e'
+    }
+
+    stage('Jar') {
+      STAGE = env.STAGE_NAME
+      sh './mvnw clean package -e'
+    }
+  } else {
+    figlet 'Delivery Continuo'
+    
+    stage('Run') {
+      STAGE = env.STAGE_NAME
+      sh 'nohup bash mvnw spring-boot:run &'
+      sleep 20
+    }
+    
+    stage('TestApp') {
+      STAGE = env.STAGE_NAME
+      //sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+    }
+  }
 
 }
 

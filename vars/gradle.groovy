0f2bffc37f36d5f05fd315ef5837bda701405f70
tomasdcm <65799599+tomasdcm@users.noreply.github.com>
2022-01-30 17:09:56 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,20 +1,39 @@
-def call() {
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
 
-        stage('Build') {
-            steps {
-                echo 'Building..'
-            }
+def call(){
+  
+  stage('TestBuild') {            
+            		bat "gradle Build"
         }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
+        
+        stage('SonarQube analysis') {
+            
+                    def scannerHome = tool 'sonar-scanner';
+                    withSonarQubeEnv('sonar-server') { 
+                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
+                    }
+           
         }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying..'
+        
+        
+
+         stage('Run') {           
+                    bat "gradle bootRun "           
         }
-    }
+        
+        /*stage('Test Application') {           
+                    bat "curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing"             
+        }*/
+        
+        stage('Nexus') {    
+                   echo 'Nexus stage'
+                   // bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
+        }
+
 }
 
 return this;

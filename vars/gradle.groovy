4f3999c18e345a3275e5e4347e8c1431a1980d26
tomasdcm <65799599+tomasdcm@users.noreply.github.com>
2022-01-30 20:33:39 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -4,36 +4,57 @@
 	ejecucion.call()
 */
 
-def call(){
-  
-  stage('TestBuild') {            
-            	// bat "gradle Build"
-        }
-        
-        stage('SonarQube analysis') {
-            
-                    /*def scannerHome = tool 'sonar-scanner';
-                    withSonarQubeEnv('sonar-server') { 
-                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build "
-                    }*/
-           
-        }
-        
-        
+def call(String pipelineType){
 
-         stage('Run') {           
-                    //bat "gradle bootRun "           
-        }
-        
-        /*stage('Test Application') {           
-                    bat "curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing"             
-        }*/
-        
-        stage('Nexus') {    
-                   echo 'Nexus stage'
-                   // bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
-        }
+	figlet 'Gradle'
 
+	if (pipelineType == 'CI'){
+		figlet 'Integracion Continua'
+
+		stage('Build & Unit Test'){
+			STAGE = env.STAGE_NAME
+			figlet "Stage: ${env.STAGE_NAME}"
+		}
+
+		stage('Sonar'){
+			STAGE = env.STAGE_NAME
+			figlet "Stage: ${env.STAGE_NAME}"
+		}
+
+		stage('Run'){
+			STAGE = env.STAGE_NAME
+			figlet "Stage: ${env.STAGE_NAME}"
+		}
+
+		stage('Test'){
+			STAGE = env.STAGE_NAME
+			figlet "Stage: ${env.STAGE_NAME}"
+		}
+
+		stage('UploadSnapshotJar'){
+			STAGE = env.STAGE_NAME
+			figlet "Stage: ${env.STAGE_NAME}"
+		}
+	} else {
+		figlet 'Delivery Continuo'
+
+		stage('DownloadSnapshotJar'){
+			STAGE = env.STAGE_NAME
+			figlet "Stage: ${env.STAGE_NAME}"
+		}
+		stage('RunSnapshotJar'){
+			STAGE = env.STAGE_NAME
+			figlet "Stage: ${env.STAGE_NAME}"
+		}
+		stage('TestSnapshotJar'){
+			STAGE = env.STAGE_NAME
+			figlet "Stage: ${env.STAGE_NAME}"
+		}
+		stage('UploadReleaseJar'){
+			STAGE = env.STAGE_NAME
+			figlet "Stage: ${env.STAGE_NAME}"
+		}
+	}
 }
 
 return this;

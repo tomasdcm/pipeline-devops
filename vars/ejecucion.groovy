04620b570afc81742b734c754286ebbeb36dc132
tomasdcm <65799599+tomasdcm@users.noreply.github.com>
2022-01-30 20:34:47 -0300

    Update ejecucion.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -6,35 +6,56 @@
 
 def call(){
   
-  pipeline {
+	pipeline {
 
-	agent any
+		agent any
+		
+		environment {
+		    STAGE = ''
+		}
 
-	parameters {
-  		choice choices: ['gradle', 'maven'], description: 'indicar herramienta de construccion', name: 'builTools'
-	}
+		parameters {
+			choice(name: 'buildTool', choices: ['gradle', 'maven'], description: 'Indicar herramienta de construcción')
+		}
 
-	stages{
-		stage('Pipeline'){
-			steps{
-				script{				    
-					figlet "Pipeline"
+		stages{
+			stage('Pipeline'){
+				steps{
+					script{
+						println 'Pipeline'
 
-					if(params.builTools == 'gradle')
-					{
-					   gradle()
-					}
-					else
-					{
-					   maven()
+						if (params.buildTool == "gradle") {
+							gradle(verifyBranchName())
+						} else {
+							maven(verifyBranchName())
+						}
+
 					}
 				}
 			}
-		}		
-		
+		}
+
+		post {
+			success {
+				slackSend color: 'good', message: "[${env.USER}][${env.JOB_NAME}][${params.buildTool}] Ejecución exitosa."
+			}
+
+			failure {
+				slackSend color: 'danger', message: "[${env.USER}][${env.JOB_NAME}][${params.buildTool}] Ejecución fallida en stage ${STAGE}."
+				error "Ejecución fallida en stage ${STAGE}"
+			}
+		}
 	}
+
 }
 
+
+def verifyBranchName(){
+	if (env.GIT_BRANCH.contains('feature-') || env.GIT_BRANCH.contains('develop')){
+		return 'CI'
+	} else {
+		return 'CD'
+	}
 }
 
 return this;

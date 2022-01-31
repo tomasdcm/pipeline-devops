c56aadf4f2e323de6dcc07fb6aeb6e57c9de401d
tomasdcm <65799599+tomasdcm@users.noreply.github.com>
2022-01-31 20:38:01 -0300

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -30,17 +30,17 @@ def call(){
 		if(str.contains('compile') || params.stage.isEmpty() )
 		{	
 			stage('Compile Code') {            
-            			//bat "mvn clean compile -e"
+            			bat "mvn clean compile -e"
         		}
 		}
 
 		if(str.contains('sonar') || params.stage.isEmpty())
 		{
 			stage('SonarQube analysis') {            
-				    /*def scannerHome = tool 'sonar-scanner';
+				    def scannerHome = tool 'sonar-scanner';
 				    withSonarQubeEnv('sonar-server') { 
 				    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build "
-			    }*/
+			    }
            
         		}
 		}
@@ -48,28 +48,28 @@ def call(){
 		if(str.contains('test') || params.stage.isEmpty())
 		{
 			stage('Test Code') {           
-				//bat "mvn clean test -e" 
+				bat "mvn clean test -e" 
 			}
 		}
 		
 		if(str.contains('code') || params.stage.isEmpty())
 		{
 			stage('Jar Code') {           
-                    		//bat "mvn clean package -e"            
+                    		bat "mvn clean package -e"            
         		}
 		}
 		
 		if(str.contains('run') || params.stage.isEmpty())
 		{
 			stage('Run Jar') {           
-                    		//bat "start /min mvn spring-boot:run &"           
+                    		bat "start /min mvn spring-boot:run &"           
         		}
 		}
 
 		if(str.contains('nexus') || params.stage.isEmpty())
 		{
 			stage('Nexus') {            
-				//bat "curl -v --user admin:holamundo --upload-file C:/Users/Tomás/.jenkins/workspace/pipeline_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
+				bat "curl -v --user admin:holamundo --upload-file C:/Users/Tomás/.jenkins/workspace/pipeline_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://http://8e4a-181-43-194-204.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
 			}
 		}
 

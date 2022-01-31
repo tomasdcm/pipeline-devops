054187ad849f44ee365986eb09c86b6f28a0a41c
tomasdcm <65799599+tomasdcm@users.noreply.github.com>
2022-01-31 19:08:11 -0300

    Update ejecucion.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -7,7 +7,7 @@ def call(){
 
 	parameters {
   		choice choices: ['gradle', 'maven'], description: 'Favor indicar herramienta de construccion', name: 'builTools'
-		string(defaultValue: '', name: 'Favor ingresar la(s) etapas a ejecutar, separando con ;', trim: true)
+		string(defaultValue: '', name: 'stage', description: 'Favor ingresar la(s) etapas a ejecutar, separando con ;', trim: true)
 		
 		
 	}

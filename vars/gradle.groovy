bc4d261ad7266e1d391bf2bc81fecd11f60aa47d
tomasdcm <65799599+tomasdcm@users.noreply.github.com>
2022-01-31 19:05:17 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,39 +1,63 @@
-/*
-	forma de invocación de método call:
-	def ejecucion = load 'script.groovy'
-	ejecucion.call()
-*/
-
 def call(){
-  
-  stage('TestBuild') {            
-            	// bat "gradle Build"
-        }
-        
-        stage('SonarQube analysis') {
-            
-                    /*def scannerHome = tool 'sonar-scanner';
-                    withSonarQubeEnv('sonar-server') { 
-                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build "
-                    }*/
-           
-        }
-        
-        
+	
+	String[] str
+	str = params.stage.split(';')
+  	println str.size()
+	println str[0]
+		
+	def flag = true
+	for (int i = 0; i < str.size(); i++) {
+		switch(str[0]) {
+			case "build":		   
+			case "sonar":
+			case "run":
+			case "nexus":
+			case "":
+			    flag = true
+		  
+			 default:
+			    flag = false
+			    break
+		}	
+	}
+	
+	println flag
+	if(flag){
+		if(str.contains('build') || params.stage.isEmpty() )
+		{	
+			stage('TestBuild') {            
+				// bat "gradle Build"
+			}
+		}
 
-         stage('Run') {           
-                    //bat "gradle bootRun "           
-        }
-        
-        /*stage('Test Application') {           
-                    bat "curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing"             
-        }*/
-        
-        stage('Nexus') {    
-                   echo 'Nexus stage'
-                   // bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
-        }
+		if(str.contains('sonar') || params.stage.isEmpty())
+		{
+			stage('SonarQube analysis') {            
+				    /* def scannerHome = tool 'sonar-scanner';
+				    withSonarQubeEnv('sonar-server') { 
+				    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
+				    }  */         
+			}
+		}
 
+		if(str.contains('run') || params.stage.isEmpty())
+		{
+			stage('Run') {           
+				// bat "gradle bootRun "           
+			}
+		}
+
+		if(str.contains('nexus') || params.stage.isEmpty())
+		{
+			stage('Nexus') {            
+				// bat "curl -v --user admin:holamundo --upload-file C:/Users/Tomás/.jenkins/workspace/pipeline_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
+			} 
+		}
+
+	}
+	else{
+		println 'Ingresó una o más stages que no existen. Favor corregir y volver a construir pipeline.'
+	}	
 }
 
 return this;

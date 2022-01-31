b1352520b3f11e64650f9411a176c09eb571b9dd
tomasdcm <65799599+tomasdcm@users.noreply.github.com>
2022-01-31 20:35:39 -0300

    Update gradle.groovy

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -23,31 +23,31 @@ def call(){
 		if(str.contains('build') || params.stage.isEmpty() )
 		{	
 			stage('TestBuild') {            
-				// bat "gradle Build"
+				bat "gradle Build"
 			}
 		}
 
 		if(str.contains('sonar') || params.stage.isEmpty())
 		{
 			stage('SonarQube analysis') {            
-				    /* def scannerHome = tool 'sonar-scanner';
+				    def scannerHome = tool 'sonar-scanner';
 				    withSonarQubeEnv('sonar-server') { 
 				    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
-				    }  */         
+				    }         
 			}
 		}
 
 		if(str.contains('run') || params.stage.isEmpty())
 		{
 			stage('Run') {           
-				// bat "gradle bootRun "           
+				bat "gradle bootRun "           
 			}
 		}
 
 		if(str.contains('nexus') || params.stage.isEmpty())
 		{
 			stage('Nexus') {            
-				// bat "curl -v --user admin:holamundo --upload-file C:/Users/Tomás/.jenkins/workspace/pipeline_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
+				bat "curl -v --user admin:holamundo --upload-file C:/Users/Tomás/.jenkins/workspace/pipeline_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://8e4a-181-43-194-204.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
 			} 
 		}
 

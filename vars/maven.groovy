3c387b89900b4062802aa06e9f34f07075d7f725
tomasdcm <65799599+tomasdcm@users.noreply.github.com>
2022-01-31 19:39:27 -0300

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -36,41 +36,41 @@ def call(){
 
 		if(str.contains('sonar') || params.stage.isEmpty())
 		{
-			stage('SonarQube analysis') {            
+			/*stage('SonarQube analysis') {            
 				    def scannerHome = tool 'sonar-scanner';
 				    withSonarQubeEnv('sonar-server') { 
 				    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
 			    }
            
-        		}
+        		}*/
 		}
 
 		if(str.contains('test') || params.stage.isEmpty())
 		{
-			stage('Test Code') {           
+			/*stage('Test Code') {           
 				bat "mvn clean test -e" 
-			}
+			}*/
 		}
 		
 		if(str.contains('code') || params.stage.isEmpty())
 		{
-			stage('Jar Code') {           
+			/*stage('Jar Code') {           
                     		bat "mvn clean package -e"            
-        		}
+        		}*/
 		}
 		
 		if(str.contains('run') || params.stage.isEmpty())
 		{
-			 stage('Run Jar') {           
+			/*stage('Run Jar') {           
                     		bat "start /min mvn spring-boot:run &"           
-        		}
+        		}*/
 		}
 
 		if(str.contains('nexus') || params.stage.isEmpty())
 		{
-			stage('Nexus') {            
+			/*stage('Nexus') {            
 				bat "curl -v --user admin:holamundo --upload-file C:/Users/Tomás/.jenkins/workspace/pipeline_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
-			} 
+			}*/
 		}
 
 	}

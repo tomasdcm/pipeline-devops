3f90ccd48815146e569768480b806d842316d6e6
tomasdcm <65799599+tomasdcm@users.noreply.github.com>
2022-01-31 19:20:30 -0300

    Update maven.groovy

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,49 +1,82 @@
-/*
-	forma de invocación de método call:
-	def ejecucion = load 'script.groovy'
-	ejecucion.call()
-*/
-
 def call(){
-  
-  		stage('Compile Code') {            
-            		bat "mvn clean compile -e"
-        }
-        
-        stage('SonarQube analysis') {
-            
-                    def scannerHome = tool 'sonar-scanner';
-                    withSonarQubeEnv('sonar-server') { 
-                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
-                    }
-           
-        }
-        
-         stage('Test Code') {           
-                    bat "mvn clean test -e" 
-        }
+	
+	
+	String[] str	
+      	str = params.stage.split(';')
+	
+	
+	def flag = true
+	for (int i = 0; i < str.size(); i++) {
+		switch(str[0]) {
+			case "compile":		   
+			case "sonar":
+			case "test":
+			case "code":
+			case "run":
+			case "nexus":
+			case "":
+			    flag = true
+		  
+			 default:
+			    flag = false
+			    break
+		}	
+	}
+	
+	
+	
+	if(flag)
+	{
+		if(str.contains('compile') || params.stage.isEmpty() )
+		{	
+			stage('Compile Code') {            
+            			bat "mvn clean compile -e"
+        		}
+		}
 
-         stage('Jar Code') {
+		if(str.contains('sonar') || params.stage.isEmpty())
+		{
+			stage('SonarQube analysis') {            
+				    def scannerHome = tool 'sonar-scanner';
+				    withSonarQubeEnv('sonar-server') { 
+				    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build " 
+			    }
            
-                    bat "mvn clean package -e"            
-        }
+        		}
+		}
 
-         stage('Run Jar') {           
-                    bat "start /min mvn spring-boot:run &"           
-        }
-        
-        /*stage('Test Application') {
-            steps {
-                script{
-                    bat "start chrome http://localhost:8081/rest/mscovid/test?msg=testing" 
-                }                                    
-            }
-        }*/
-        
-        stage('Upload Nexus') {   
-                       echo 'Nexus stage'
-                   // bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
-        }
+		if(str.contains('test') || params.stage.isEmpty())
+		{
+			stage('Test Code') {           
+				bat "mvn clean test -e" 
+			}
+		}
+		
+		if(str.contains('code') || params.stage.isEmpty())
+		{
+			stage('Jar Code') {           
+                    		bat "mvn clean package -e"            
+        		}
+		}
+		
+		if(str.contains('run') || params.stage.isEmpty())
+		{
+			 stage('Run Jar') {           
+                    		bat "start /min mvn spring-boot:run &"           
+        		}
+		}
+
+		if(str.contains('nexus') || params.stage.isEmpty())
+		{
+			stage('Nexus') {            
+				bat "curl -v --user admin:123456 --upload-file C:/Users/nmt02/.jenkins/workspace/pipilene_sonar_feature-sonar/build/DevOpsUsach2020-0.0.1.jar http://7fb6-186-79-184-102.ngrok.io/repository/test-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar "            
+			} 
+		}
+
+	}
+	else{
+		println 'Ingresó una o más stages que no existen. Favor corregir y volver a construir pipeline.'
+	}
 
 }
 
